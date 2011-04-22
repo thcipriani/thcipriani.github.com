7fdc228af46e0f356251da596c070f821d28c71b
Tyler Cipriani <thcipriani@gmail.com>
2011-04-22 14:49:28 -0600

    HTML5 Game Dev

---
+++ html5gamedev/detectKey.js
@@ -0,0 +1,34 @@
+var watchKeys = [37,38,39,40];
+var pressed;
+
+window.addEventListener("keydown", function(e){
+
+  var keyIndex = jQuery.inArray(e.keyCode, watchKeys);
+
+  switch( keyIndex ){
+
+    case 0:
+      //console.log("Left Arrow");
+      pressed = "Left Arrow";
+      break;
+
+    case 1:
+      //console.log("Up Arrow");
+      pressed = "Up Arrow";
+      break;
+
+    case 2:
+      //console.log("Right Arrow");
+      pressed = "Right Arrow";
+      break;
+
+    case 3:
+      //console.log("Down Arrow");
+      pressed = "Down Arrow";
+      break;
+
+  }  
+
+  $("body").text("You pressed the " + pressed);
+  
+});
