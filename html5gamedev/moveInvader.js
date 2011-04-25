cb585246848e33eb3e299e4f0171892f702b947e
Tyler Cipriani <thcipriani@gmail.com>
2011-04-25 15:11:29 -0600

    jquery wrestling

--- html5gamedev/moveInvader.js
+++ html5gamedev/moveInvader.js
@@ -7,31 +7,35 @@ var x = 0, y = 0;
   
     var keyIndex = jQuery.inArray(e.keyCode, watchKeys);
   
-    switch( keyIndex ){
-  
-      case 0:
-        (x > 0) ? x-=1 : x = 0;
-        //console.log("Left Arrow");
-        continue;
-  
-      case 1:
-        (y > 0) ? y-=1 : y=0;
-        //console.log("Up Arrow");
-        continue;
-  
-      case 2:
-        x += 1;
-        //console.log("Right Arrow");
-        continue;
-  
-      case 3:
-        y += 1;
-        //console.log("Down Arrow");
-        continue;
-  
+    if (keyIndex > -1 && keyIndex < 4){
+
+      switch( keyIndex ){
+    
+        case 0:
+          (x > 0) ? x-=1 : x = 0;
+          //console.log("Left Arrow");
+          break;
+    
+        case 1:
+          (y > 0) ? y-=1 : y=0;
+          //console.log("Up Arrow");
+          break;
+    
+        case 2:
+          x += 1;
+          //console.log("Right Arrow");
+          break;
+    
+        case 3:
+          y += 1;
+          //console.log("Down Arrow");
+          break;
+    
+      }  
+
       $("body").append(x+", "+y);
-    }  
-  
+
+    }
   
   });
 });
