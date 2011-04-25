8360cd563286440c6c52e8b124b98645ea173ee5
Tyler Cipriani <thcipriani@gmail.com>
2011-04-25 15:08:14 -0600

    test

--- html5gamedev/moveInvader.js
+++ html5gamedev/moveInvader.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 var watchKeys = [37,38,39,40];
-var x, y = 0;
+var x = 0, y = 0;
 
   $(document).keydown(function(e){
   
@@ -10,36 +10,28 @@ var x, y = 0;
     switch( keyIndex ){
   
       case 0:
+        (x > 0) ? x-=1 : x = 0;
         //console.log("Left Arrow");
-        if (x > 0){
-          x -= 1;
-        } else {
-          x = 0;
-        }
-        break;
+        continue;
   
       case 1:
-        if (y > 0){
-          y -= 1;
-        } else {
-          y = 0;
-        }
+        (y > 0) ? y-=1 : y=0;
         //console.log("Up Arrow");
-        break;
+        continue;
   
       case 2:
         x += 1;
         //console.log("Right Arrow");
-        break;
+        continue;
   
       case 3:
         y += 1;
         //console.log("Down Arrow");
-        break;
+        continue;
   
+      $("body").append(x+", "+y);
     }  
   
-    $("body").append(x+", "+y);
   
   });
 });
