5378f8e7d57e59df9ea4d706bc2ca263da3cb1e3
Tyler Cipriani <thcipriani@gmail.com>
2011-04-25 17:17:33 -0600

    little harder - revision 1

--- html5gamedev/mouseMoveInvader.js
+++ html5gamedev/mouseMoveInvader.js
@@ -4,11 +4,11 @@ var invader = $("#container > img");
 
 /* Easy way to set invader's position to the middle of the window - 
    using percentages in css mess with 'animate' function */
-  invader.css({
-    "position" : "relative",
-    "top" : $(window).height()/2 - invader.height()/2 + "px",
-    "left" : $(window).width()/2 - invader.width()/2 + "px"
-    });
+invader.css({
+  "position" : "relative",
+  "top" : $(window).height()/2 - invader.height()/2 + "px",
+  "left" : $(window).width()/2 - invader.width()/2 + "px"
+});
 
 var rightEdge = $(window).width() - invader.width();
 var bottomEdge = $(window).height() - invader.height();
@@ -16,9 +16,16 @@ var position = invader.offset();
 var x = position.top; 
 var y = position.left;
 
-  console.log(x+", "+y);
+$(document).mousemove(function(e){
+  var x = e.pageX + 15 + "px";
+  var y = e.pageY + 15 + "px";
 
-  
+  invader.animate({
+    "top" : y,
+    "left" : x
+  }, 1, 'linear');
+
+});  
 
  
 });
