45b88b3a5012db5cf24145a184a56f51a0caf20c
Tyler Cipriani <thcipriani@gmail.com>
2011-04-27 13:42:53 -0600

    P2PU Updates

--- html5gamedev/mouseMoveInvader.js
+++ html5gamedev/mouseMoveInvader.js
@@ -18,14 +18,17 @@ var y = position.left;
 
 $(document).mousemove(function(e){
 
-  var x = e.pageX - invader.width()/2 + "px";
-  var y = e.pageY - invader.height()/2 + "px";
-
-    invader.animate({
-      "top" : y,
-      "left" : x
-    }, 10, 'linear');
+  x = e.pageX - invader.width()/2 + "px";
+  y = e.pageY - invader.height()/2 + "px";
 
 });  
- 
+
+window.setInterval(function(){
+  invader.animate({
+    "top" : y,
+    "left" : x
+  }, 1, 'linear');
+  }, 1);
+
+
 });
