946d58060235602d3d29e24a5e7813e7674ed544
Tyler Cipriani <thcipriani@gmail.com>
2011-05-04 12:52:49 -0600

    Steal this code

--- python101/plagiarise.py
+++ python101/plagiarise.py
@@ -4,7 +4,6 @@
 
 import sys
 import urllib2
-from urllib import urlretrieve
 
 """
 P2PU Python 101- Chapter 5 plagerism
@@ -14,24 +13,30 @@ def bookem(file):
   count = 0
   word = []
   yourwords = []
-  try:
-    f = open(yourFile, 'rU')
-    for line in f:
-      words = line.split()
-      for word in words:
-        yourwords.append(word)
-  except IOError:
-    print 'Enter a valid file'
+  theirwords = []
+
+  #Retrieve the flow control page from the python site and create a list with all words
   pythonFlowControl = "http://docs.python.org/tutorial/controlflow.html"
   pagecontent = urllib2.urlopen(pythonFlowControl)
   for line in pagecontent:
     pageWords = line.split()
     for i in pageWords:
-      if i in yourwords:
-        print 'You stole the word',i
-        count += 1
-  print count
+      theirwords.append(i)
 
+  try:
+    #open your file and see if each word is in the array you created previously
+    f = open(yourFile, 'rU')
+    for line in f:
+      words = line.split()
+      for word in words:
+        if word in theirwords:
+          print 'You stole',word
+          count += 1
+
+  except IOError:
+    print 'Enter a valid file'
+
+  print 'IN ALL YOU\'VE STOLEN',count,'WORDS'
 
 def main():
   args = sys.argv[1:]
